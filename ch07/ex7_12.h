6722afc46f2a8b62a847746dcb6f169b0d53f1d8
RaynorRaider <573426433@qq.com>
2016-04-07 13:07:49 +0800

    Update ex7_12.h
    origin code have a linking error in vs2013

--- ch07/ex7_12.h
+++ ch07/ex7_12.h
@@ -11,14 +11,15 @@
 #include <string>
 #include <iostream>
 
-struct Sales_data;
-std::istream &read(std::istream&, Sales_data&);
-
 struct Sales_data {
     Sales_data() = default;
     Sales_data(const std::string &s):bookNo(s) { }
     Sales_data(const std::string &s, unsigned n, double p):bookNo(s), units_sold(n), revenue(n*p){ }
-    Sales_data(std::istream &is) { read(is, *this); }
+    Sales_data(std::istream &is) 
+    { 
+        std::istream &read(std::istream &is, Sales_data &item);
+        read(is, *this); 
+    }
     
     std::string isbn() const { return bookNo; };
     Sales_data& combine(const Sales_data&);
